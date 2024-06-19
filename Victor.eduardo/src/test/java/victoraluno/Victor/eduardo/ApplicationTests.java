d390d66a0909da6a69d78d26dadf38d5ae383443
Victor Eduardo <127709638+VictoreduardoSi@users.noreply.github.com>
2024-06-19 01:44:20 -0300

    Add files via upload

---
+++ Victor.eduardo/src/test/java/victoraluno/Victor/eduardo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package victoraluno.Victor.eduardo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
